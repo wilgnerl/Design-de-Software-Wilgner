6fe9b0dbc75a1ac104687ddca9964c97663cc14b
Fabricio2530 <39420630+Fabricio2530@users.noreply.github.com>
2020-03-30 01:38:35 -0300

    Field atualizado

--- field.py
+++ field.py
@@ -12,37 +12,35 @@ fichas = 100
 #escolha é uma flag para o While
 FIL = True
 while FIL:
-    pergunta1 = input("Você deseja apostar ou sair? ")
+                dado1 = random.randint(1,6)
+                dado2 = random.randint(1,6)
+                resultado = dado1 + dado2 
+                print('Resultado:',resultado)
+                time.sleep(1)
 
-    if pergunta1 == 'sair':
-            FIL = False
-    if pergunta1 != 'sair':
-        #fazendo o sorteio dos dados e a soma dos resultados
-            dado1 = random.randint(1,6)
-            dado2 = random.randint(1,6)
-            resultado = dado1 + dado2     
-        #verificando as regras do jogo com base no resultado do sorteio
-            if resultado == 5 or resultado == 6 or resultado == 7 or resultado == 8:
-                print("Você perdeu")
-                fichas = fichas - aposta
-                print('Agora você tem: ', fichas)
-                FIL = False
-            elif resultado == 3 or resultado == 4 or resultado == 9 or resultado == 10 or resultado == 11:
-                print('você ganhou')
-                fichas = fichas + apostas
-                print('Agora você tem: ', fichas)
-                FIL = False
-            elif resultado == 2:
-                print('você ganhou')
-                fichas = fichas + 2*aposta
-                print('Agora você tem: ', fichas)
-                FIL = False
-            elif resultado == 12:
-                print('você ganhou')
-                fichas = fichas + 3*aposta
-                print('Agora você tem: ', fichas)
-                FIL = False
+            #verificando as regras do jogo com base no resultado do sorteio
+                if resultado == 5 or resultado == 6 or resultado == 7 or resultado == 8:
+                    print("Você perdeu")
+                    fichas = fichas - aposta
+                    print('Agora você tem: ', fichas)
+                    FIL = False
 
-        
+                elif resultado == 3 or resultado == 4 or resultado == 9 or resultado == 10 or resultado == 11:
+                    print('você ganhou')
+                    fichas = fichas + aposta
+                    print('Agora você tem: ', fichas)
+                    FIL = False
+
+                elif resultado == 2:
+                    print('você ganhou')
+                    fichas = fichas + 2*aposta
+                    print('Agora você tem: ', fichas)
+                    FIL = False
+                    
+                elif resultado == 12:
+                    print('você ganhou')
+                    fichas = fichas + 3*aposta
+                    print('Agora você tem: ', fichas)
+                    FIL = False
 
 
