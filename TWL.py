178d04e268ab27c3b7e8cf939a368fd2818d1feb
Fabricio2530 <39420630+Fabricio2530@users.noreply.github.com>
2020-03-29 21:31:56 -0300

    Finalizando o Twelve

--- TWL.py
+++ TWL.py
@@ -8,7 +8,6 @@ resultado = 0
 aposta = 0
 fichas = 100
 
-#escolha é uma flag para o While
 TWL = True
 while TWL:
     pergunta1 = input("Você deseja apostar ou sair? ")
@@ -20,7 +19,15 @@ while TWL:
             dado1 = random.randint(1,6)
             dado2 = random.randint(1,6)
             resultado = dado1 + dado2     
-           
+            #verificando a condição de ter ganho ou não
             if resultado == 12:
+                print('você ganhou')
                 fichas = fichas + aposta*30
-                
+                print('Agora você tem: ', fichas)
+                TWL = False
+            else: 
+                print('você perdeu')
+                fichas = fichas -  aposta
+                print('Agora você tem: ', fichas)
+                TWL = False
+            
